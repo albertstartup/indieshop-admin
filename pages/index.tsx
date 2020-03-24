65481cd5c6ebd3c53328456d7e6113da69f9a054
Albert Martinez <albertstartup@gmail.com>
2020-03-23 21:47:55 -0700

    feat: add drawer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { AppBar, Toolbar, Drawer } from "@material-ui/core";
 
 export default function Index() {
   return (
     <div>
+      <AppBar elevation={0}>
+        <Toolbar>Test</Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        open>
+          <div style={{width: "240px"}}></div>
+        </Drawer>
       <Title>Hello Next.js</Title>
     </div>
   );
